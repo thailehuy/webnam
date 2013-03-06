2b965de3c306252aad6f5ccbc6420927c3eb5681
Bhanu <bhanurorkitchen2@gmail.com>
2013-03-06 04:37:38 +0100

    Fix products routes

--- vendor/extensions/sites/app/controllers/refinery/sites/products_controller.rb
+++ vendor/extensions/sites/app/controllers/refinery/sites/products_controller.rb
@@ -12,7 +12,7 @@ module Refinery
       end
 
       def show
-        @product = Product.find(params[:id])
+        @product = Product.find(params[:product_id])
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @product in the line below:
