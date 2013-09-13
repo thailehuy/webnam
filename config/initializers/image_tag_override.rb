ee6cccae92b609cddb1c1f5a477d698001140004
Huy Thai <thailehuy@gmail.com>
2013-09-13 02:23:40 +0200

    Fix stuff

--- config/initializers/image_tag_override.rb
+++ config/initializers/image_tag_override.rb
@@ -1,11 +1,24 @@
 module ActionView
   module Helpers
     module AssetTagHelper
-      def image_tag(source, options = {})
+      def image_tag(source, options={})
+        options = options.symbolize_keys
+
+        src = options[:src] = path_to_image(source)
+
+        unless src =~ /^(?:cid|data):/ || src.blank?
+          options[:alt] = options.fetch(:alt){ image_alt(src) }
+        end
+
         if @site
           options[:alt] = @site.seo_image_tags.to_s.split(",").map(&:strip).sample
         end
-        options[:src] = source
+
+        if size = options.delete(:size)
+          options[:width], options[:height] = size.split("x") if size =~ %r{\A\d+x\d+\z}
+          options[:width] = options[:height] = size if size =~ %r{\A\d+\z}
+        end
+
         tag("img", options)
       end
     end
